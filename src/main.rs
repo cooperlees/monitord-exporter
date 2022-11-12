e5ac8d5692e62ce1354e4d53e33ba486b1909a65
Cooper Ry Lees <me@wcooperlees.com>
2022-11-12 22:53:10 +0000

    Add port arg, signal handler and add prom_exporter bind

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
+use std::thread;
+
 use anyhow::Result;
 use clap::Parser;
 use clap_verbosity_flag::InfoLevel;
 use log::info;
 
-const LONG_ABOUT: &str = "prometheus export to share how happy your systemd is ! 😊";
+const LONG_ABOUT: &str = "prometheus exporter to share how happy your systemd is ! 😊";
 
 /// Clap CLI Args struct with metadata in help output
 #[derive(Debug, Parser)]
 #[clap(author, version, about, long_about = LONG_ABOUT)]
 struct Cli {
-    /// Number that brings you luck
-    #[clap(short, long, value_parser, default_value_t = 69)]
-    lucky_number: u8,
+    /// TCP Port to listen on
+    #[clap(short, long, value_parser, default_value_t = 1)]
+    port: u16,
     #[clap(flatten)]
     verbose: clap_verbosity_flag::Verbosity<InfoLevel>,
+}
+
+/// Signal handler to exit cleanly
+fn signal_handler() {
+    let mut signals = match signal_hook::iterator::Signals::new([signal_hook::consts::SIGINT]) {
+        Ok(sig) => sig,
+        Err(err) => panic!("Unable to handle SIGINT: {:#?}", err),
+    };
+    for sig in signals.forever() {
+        // TODO: Print signal name somehow ...
+        info!("Received signal {:?}", sig);
+        if sig == signal_hook::consts::SIGINT {
+            std::process::exit(0);
+        }
+    }
 }
 
 fn main() -> Result<()> {
@@ -22,7 +39,13 @@ fn main() -> Result<()> {
         .filter_level(args.verbose.log_level_filter())
         .init();
 
-    info!("monitord-exporter coming soon!");
+    info!("Starting monitord-exporter on port {}", args.port);
+
+    let bind_uri = format!("[::]:{}", args.port);
+    let binding = bind_uri.parse().unwrap();
+    let _exporter = prometheus_exporter::start(binding).unwrap();
+
+    thread::spawn(signal_handler);
 
     Ok(())
 }
